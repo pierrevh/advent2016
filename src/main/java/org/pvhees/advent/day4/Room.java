b541baa270a9c6cb05ba10ca9e855a6e7ac14d68
Pierre van Hees <pvhees@xs4all.nl>
2016-12-08 17:02:15 +0100

    Day 4 part 1 excluding checksum checking

--- src/main/java/org/pvhees/advent/day4/Room.java
+++ src/main/java/org/pvhees/advent/day4/Room.java
@@ -1,7 +1,6 @@
 package org.pvhees.advent.day4;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static java.util.function.Function.identity;
@@ -18,24 +17,62 @@ public class Room {
         int startOfChecksum = roomData.indexOf("[");
         int startOfSectorId = roomData.lastIndexOf("-");
         this.data = roomData.substring(0, startOfSectorId).replaceAll("-", "");
-        System.out.println(this.data);
-        this.sectorId = Integer.parseInt(roomData.substring(startOfSectorId+1, startOfChecksum));
-        this.checksum = roomData.substring(startOfChecksum+1, roomData.length()-1);
+        this.sectorId = Integer.parseInt(roomData.substring(startOfSectorId + 1, startOfChecksum));
+        this.checksum = roomData.substring(startOfChecksum + 1, roomData.length() - 1);
     }
 
     public boolean isReal() {
 
         Stream<String> streamData = Stream.of(this.data);
         Map<String, Long> letterToCount = streamData
-                        .map(s -> s.split(""))
-                        .flatMap(Arrays::stream)
-                        .collect(groupingBy(identity(), counting()));
+                .map(s -> s.split(""))
+                .flatMap(Arrays::stream)
+                .collect(groupingBy(identity(), counting()));
 
-        //TODO: use lettercount to determine if checksum is valid
-        return false;
+        //TODO: works but is ugly
+        List<LetterCount> letterCountList = new ArrayList<>();
+        for (Map.Entry<String, Long> entry : letterToCount.entrySet()) {
+            letterCountList.add(new LetterCount(entry.getKey().charAt(0), entry.getValue()));
+        }
+
+        Collections.sort(letterCountList, new Comparator<LetterCount>(){
+            @Override
+            public int compare(LetterCount d1, LetterCount d2){
+                int countCmp = d1.count - d2.count < 0 ? 1 : (d1.count - d2.count > 0 ? -1 : 0);
+                if (countCmp != 0) {
+                    return countCmp;
+                }
+                return d1.letter.compareTo(d2.letter);
+            }
+        });
+
+        StringBuilder calculatedChecksum = new StringBuilder();
+        for (int i=0; i< 5; i++) {
+            calculatedChecksum.append(letterCountList.get(i).getLetter());
+        }
+
+        return calculatedChecksum.toString().equalsIgnoreCase(this.checksum);
     }
 
     public int getSectorId() {
         return this.sectorId;
+    }
+
+    class LetterCount {
+        private Character letter;
+        private long count;
+
+        public LetterCount(Character letter, long count) {
+            this.letter = letter;
+            this.count = count;
+        }
+
+        public Character getLetter() {
+            return letter;
+        }
+
+        public long getCount() {
+            return count;
+        }
     }
 }
