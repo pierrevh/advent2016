4c105411398ba2a3e2c433fa35ffc5e537c4b37f
Pierre van Hees <pvhees@xs4all.nl>
2016-12-10 10:47:35 +0100

    Make isReal() to fully use  java8 stream capabilities to make code more clear

--- src/main/java/org/pvhees/advent/day4/Room.java
+++ src/main/java/org/pvhees/advent/day4/Room.java
@@ -1,11 +1,12 @@
 package org.pvhees.advent.day4;
 
 import java.util.*;
-import java.util.stream.Stream;
 
+import static java.util.Collections.reverseOrder;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 public class Room {
 
@@ -16,66 +17,29 @@ public class Room {
     public Room(String roomData) {
         int startOfChecksum = roomData.indexOf("[");
         int startOfSectorId = roomData.lastIndexOf("-");
-        this.data = roomData.substring(0, startOfSectorId).replaceAll("-", "");
+        this.data = roomData.substring(0, startOfSectorId);
         this.sectorId = Integer.parseInt(roomData.substring(startOfSectorId + 1, startOfChecksum));
         this.checksum = roomData.substring(startOfChecksum + 1, roomData.length() - 1);
     }
 
     public boolean isReal() {
-
-        Stream<String> streamData = Stream.of(this.data);
-        Map<String, Long> letterToCount = streamData
-                .map(s -> s.split(""))
-                .flatMap(Arrays::stream)
+        Map<String, Long> letterCount = Arrays.stream(
+                this.data.replaceAll("-", "").toLowerCase().split(""))
                 .collect(groupingBy(identity(), counting()));
 
-        //TODO: works but is ugly
-        List<LetterCount> letterCountList = new ArrayList<>();
-        for (Map.Entry<String, Long> entry : letterToCount.entrySet()) {
-            letterCountList.add(new LetterCount(entry.getKey().charAt(0), entry.getValue()));
-        }
+        List<String> top5Characters = letterCount.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue(reverseOrder()).thenComparing(Map.Entry.comparingByKey()))
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .collect(toList());
 
-        Collections.sort(letterCountList, new Comparator<LetterCount>(){
-            @Override
-            public int compare(LetterCount d1, LetterCount d2) {
-                // first sort on descending count
-                int countCmp = d1.count - d2.count < 0 ? 1 : (d1.count - d2.count > 0 ? -1 : 0);
-                if (countCmp != 0) {
-                    return countCmp;
-                }
-                // then sort on ascending letter
-                return d1.letter.compareTo(d2.letter);
-            }
-        });
+        String calculatedChecksum = top5Characters.stream().reduce("", String::concat);
 
-        // The letters of the top 5 items (which are the first 5 in the list after the sort) are used for the checksum
-        StringBuilder calculatedChecksum = new StringBuilder();
-        for (int i=0; i< 5; i++) {
-            calculatedChecksum.append(letterCountList.get(i).getLetter());
-        }
-
-        return calculatedChecksum.toString().equalsIgnoreCase(this.checksum);
+        return calculatedChecksum.equalsIgnoreCase(this.checksum);
     }
 
     public int getSectorId() {
         return this.sectorId;
     }
 
-    class LetterCount {
-        private Character letter;
-        private long count;
-
-        public LetterCount(Character letter, long count) {
-            this.letter = letter;
-            this.count = count;
-        }
-
-        public Character getLetter() {
-            return letter;
-        }
-
-        public long getCount() {
-            return count;
-        }
-    }
 }
